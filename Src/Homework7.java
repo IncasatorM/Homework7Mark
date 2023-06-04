6bc571bbf3c9b2c2f9f0521ddf8ddf8a340860d2
IncasatorM <mkaminskyi97@gmail.com>
2023-06-04 14:33:07 +0300

    This is my homework with modifications

--- Src/Homework7.java
+++ Src/Homework7.java
@@ -2,5 +2,6 @@ public class Homework7 {
     public static void main(String[] args ){
 
         System.out.println("Hello Git");
+        System.out.println("Hello Git" + " я хочу учиться");
     }
 }
